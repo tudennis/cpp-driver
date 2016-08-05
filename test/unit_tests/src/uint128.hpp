b368e4290f8fadd650762aa125c7d932ff675723
Michael Penick <michael.penick@datastax.com>
2016-08-05 16:08:28 -0700

    Remove C++0x specific functionality from numeric::uint128

--- test/unit_tests/src/uint128.hpp
+++ test/unit_tests/src/uint128.hpp
@@ -1,18 +1,18 @@
 /*
  * The MIT License (MIT)
- * 
+ *
  * Copyright (c) 2015 Evan Teran
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -29,7 +29,7 @@
 #include <string>
 #include <iostream>
 #include <climits>
-#include <cstdint>
+#include <stdint.h>
 
 #include <boost/operators.hpp>
 
@@ -104,7 +104,7 @@ public:
 			int radix = 10;
 			bool minus = false;
 
-			auto i = sz.begin();
+      std::string::const_iterator i = sz.begin();
 
 			// check for minus sign, i suppose technically this should only apply
 			// to base 10, but who says that -0x1 should be invalid?
